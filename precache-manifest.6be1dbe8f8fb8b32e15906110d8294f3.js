eea59f7378b66370b37e9d198d18b9d35e3b008f
Nacho077 <ignaciogimenez70@gmail.com>
2020-10-19 23:11:35 -0300

    Updates

---
+++ precache-manifest.6be1dbe8f8fb8b32e15906110d8294f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96dcf825a2852be1f7e4360b52f8ed1",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "a24f6ab971006a448cf1",
+    "url": "/ToDoList/static/css/main.9d49d42c.chunk.css"
+  },
+  {
+    "revision": "0089f676b9165c328ecd",
+    "url": "/ToDoList/static/js/2.8d3e488a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoList/static/js/2.8d3e488a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24f6ab971006a448cf1",
+    "url": "/ToDoList/static/js/main.5c3672f0.chunk.js"
+  },
+  {
+    "revision": "95ab476d57fadd6c0226",
+    "url": "/ToDoList/static/js/runtime-main.2b209adc.js"
+  }
+]);
